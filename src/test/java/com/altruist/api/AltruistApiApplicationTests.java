1ac9ef7a6e8923deed327406d1f1846959a48795
jlopezquilis <jlopezquilis@gmail.com>
2025-03-25 12:47:57 +0100

    API Structure

---
+++ src/test/java/com/altruist/api/AltruistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altruist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltruistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
